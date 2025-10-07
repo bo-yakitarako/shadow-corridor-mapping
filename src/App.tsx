c10207b688555bc559e68d3796a1bf458183f5d9
bo-yakitarako <bo-yakitarako@users.noreply.github.com>
2025-10-08 05:20:55 +0900

    入っちゃったあ！

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,26 @@
+import { Box, CssBaseline } from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: { mode: 'dark' },
+});
+
 export const App = () => {
-  return <div>Ahooo World</div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Box
+        component="main"
+        sx={{
+          width: '100%',
+          height: '100dvh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        おめえほんとか？
+      </Box>
+    </ThemeProvider>
+  );
 };
